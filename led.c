9708f0d4ec1f6994b9461b359702eccc2c0c0a18
ghastman <ghastman@gmail.com>
2021-01-01 16:49:37 -0600

    think i have my pinout correct, ha!!!

--- led.c
+++ led.c
@@ -21,12 +21,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 void led_set(uint8_t usb_led)
 {
-    DDRB |= (1 << 6) | (1 << 5) | (1 << 4);
+    DDRB |= (1 << 7) | (1 << 6) | (1 << 54);
 
     if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-        PORTB &= ~(1 << 4);
+        PORTB &= ~(1 << 7);
     } else {
-        PORTB |= (1 << 4);
+        PORTB |= (1 << 7);
     }
 
     if (usb_led & (1<<USB_LED_SCROLL_LOCK)) {
